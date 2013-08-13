6f85b9101db14c966ac2d8384f5dd7d3c405d792
Noel Welsh <noel@untyped.com>
2013-08-13 11:08:15 +0100

    Collect first and last name on mailing list signup

--- assets/js/mailchimp.js
+++ assets/js/mailchimp.js
@@ -1,4 +1,6 @@
-var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';
+var fnames = new Array();var ftypes = new Array();
+ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';
+
 jqueryLoaded=true;
 var head= document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
